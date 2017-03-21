b8cbab5714d3a3f31ee83fd47171b4ace0e54889
DenisPerv <denperv@gmail.com>
2017-03-21 20:13:23 +0200

    Automation test for Google Calculator using Selenium Webdriver

--- Redme.md
+++ Redme.md
@@ -1,1 +1,30 @@
 Selenium JS test for Google Calculator
++++
+date        = "2017-03-21T11:27:27-04:00"
+title       = "Selenium JS test for Google Calculator"
+description = "This test can easy send expressions in Google Calculator using Selenium Webdriver"
+tags        = [ "Google Calculator", "Node JS", "selenium" ]
+topics      = [ "Automation", "Test" ]
+project_url = "https://github.com/spf13/nitro"
++++
+
+# Overview
+
+Quick and easy automation test to verify Google Calculators work for Windows 7.
+
+# External dependencies
+
+You need
+* Node JS (v6.10.0)
+* Node JS modules: Selenium Webdriver (v2.0), Selenium Chrome Driver (v2.0)
+* Windows 7 on your machine
+
+# Usage
+
+Just change arguments in the function called mytest(driver, expression, expected result) for your test purposes.
+Result of expression compares with expected result and program returns in console "PASS" (result and expected result are same) or "FAIL" (if they are different). 
+
+Example: mytest(driver, '2 + 5', '7') means: in Google Calculator sends expression 2 + 5 and after that Google Calculator will calculate the amount. 
+Then, the test will compare amount with expected 7 and returns "PASS" in console.
+
+
---
+++ google_calculator_test.js
@@ -0,0 +1,56 @@
+var path = require('chromedriver').path
+var assert = require('assert')
+
+const {Builder, By, until} = require('selenium-webdriver')
+//IDs of the search, result fields and search button ID
+const SEARCH_FIELD_ID = 'lst-ib'
+const SEARCH_BUTTON_ID = '_fZl'
+const RESULT_FIELD_ID = 'cwos'
+
+var driver = new Builder().forBrowser('chrome').build()
+
+driver.onetest = function(expected, got) {
+// This function compares expected and actual results
+    var passed = expected === got
+
+    console.log('Expecting ' + expected +
+                ', Got ' + got + ' [' +
+                (passed ? 'PASS' : 'FAIL') +
+                ']')
+    if (!passed) {
+
+    }
+}
+
+function mytest(driver, expression, expected) {
+    driver.get('http://www.google.com/ncr')
+// Navigate to Google Calculator by sending expression in Google Search (search by Google Calculator is not working)
+    driver.wait(until.elementLocated(By.id(SEARCH_FIELD_ID)), 1000)
+//Clearing of the field
+    driver.findElement(By.id(SEARCH_FIELD_ID)).clear()
+//Sending an expression
+    driver.findElement(By.id(SEARCH_FIELD_ID)).sendKeys(expression)
+//Click on search button
+    driver.findElement(By.id(SEARCH_BUTTON_ID)).click()
+//Getting a result
+    driver.wait(until.elementLocated(By.id(RESULT_FIELD_ID)), 2000)
+    driver.findElement(By.id(RESULT_FIELD_ID)).getText().then(function(q) {
+
+        driver.onetest(expected, q)
+
+    })
+}
+
+// driver.get('http://www.google.com/ncr')
+
+// Examples of tests(you can change expression in brachets for your own test)
+// Second argument is an expression, that you want to test and third argument is expected result.
+// Positive tests
+mytest(driver, '2 + 5', '7')
+mytest(driver, '5 - 1', '4')
+mytest(driver, '6 / 2', '3')
+
+// Negative test
+mytest(driver, '5 * 8', '39')
+
+
